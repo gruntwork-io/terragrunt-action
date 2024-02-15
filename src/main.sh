2e3cdccbc39ba537a75480670d89eda38ca72b9b
Denis O <denis.o@linux.com>
2024-02-15 19:23:45 +0000

    other rw

--- src/main.sh
+++ src/main.sh
@@ -91,7 +91,7 @@ function setup_git {
 function setup_permissions {
   # Set permissions for current user
   sudo chown -R $(whoami) .
-  sudo chmod -R o+r .
+  sudo chmod -R o+rw .
   # Set permissions for the output file
   sudo chown -R $(whoami) "${GITHUB_OUTPUT}"
 }
