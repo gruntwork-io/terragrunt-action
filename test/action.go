4cbd43012cdd8c595d0eea32956b1d2dfade334b
arsci <ryan@gruntwork.io>
2024-04-19 10:17:55 -0700

    add opentofu, add mise, remove tfenv, remove tgswitch

--- test/action.go
+++ test/action.go
@@ -9,6 +9,7 @@ import (
 func buildActionImage(t *testing.T) string {
 	tag := "terragrunt-action:" + random.UniqueId()
 	buildOptions := &docker.BuildOptions{
+		Architectures: []string{"linux/amd64"},
 		Tags: []string{tag},
 	}
 	docker.Build(t, "..", buildOptions)
