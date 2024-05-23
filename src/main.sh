e0e41dfb1079b2ece838fa0999c245816be4d134
Denis O <denis.o@linux.com>
2024-05-23 21:37:05 +0100

    escaped_message

--- src/main.sh
+++ src/main.sh
@@ -90,7 +90,7 @@ function comment {
     return
   fi
   local escaped_message
-  escaped_message=$(printf '%s' "$message" | sed 's/"/\\"/g')
+  escaped_message=$(printf '%s' "$message" | sed 's/\\/\\\\/g; s/"/\\"/g')
   local tmpfile
   tmpfile=$(mktemp)
   echo "{\"body\": \"$escaped_message\"}" > "$tmpfile"
